b068ba364479932cd36aecdf5c6dd3c9859488cd
hm-zaw <hmzzzz2004@gmail.com>
2025-11-22 16:49:56 +0630

    deploy 2nd commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
 import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
@@ -41,7 +40,7 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
+          <span className="navbar-brand-text">Hmz.</span>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
@@ -107,7 +106,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/hm-zaw"
                 target="_blank"
                 className="fork-btn-inner"
               >
